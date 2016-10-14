587eb7659b6408ac43f9bbd76b9720077e9e3be8
Vijay Gaikwad <vig@microsoft.com>
2016-10-13 17:07:33 -0700

    adding columbus data csv from kaggle

---
+++ DataHelper/FileOps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHelper
+{
+    internal class FileOps
+    {
+        private string FilePath = "C:\\MySource\\Repo\\AspNetWebApp\\DataHelper\\data\\columbus.csv";
+
+
+    }
+}
