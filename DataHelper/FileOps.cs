c8699d795e9ac2a00810a1f9ef8cea5a6194029a
Vijay Gaikwad <vig@microsoft.com>
2016-10-13 17:52:01 -0700

    adding file parser

--- DataHelper/FileOps.cs
+++ DataHelper/FileOps.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
 
 namespace DataHelper
 {
@@ -10,6 +11,25 @@ namespace DataHelper
     {
         private string FilePath = "C:\\MySource\\Repo\\AspNetWebApp\\DataHelper\\data\\columbus.csv";
 
+
+        public string ReadFile()
+        {
+            using (TextFieldParser parser = new TextFieldParser(FilePath))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters("\t");
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                }
+
+            }
+
+            return null;
+        }
+
+
+        
 
     }
 }
