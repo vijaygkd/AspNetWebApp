dc07d040f43b990a83d1a3c8117caaa6b279de1f
Vijay Gaikwad <vig@microsoft.com>
2016-10-13 17:28:48 -0700

    adding log entry model

---
+++ DataModels/LogEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class LogEntry
+    {
+        public int Day { get; set; }
+
+        public string Month { get; set; }
+
+        public int Year { get; set; }
+
+        public DateTime Date
+        {
+            get
+            {
+                var dateFormat = "d MMMM YYYY";
+                var dateString = $"{this.Day} {this.Month} {this.Year}";
+                return DateTime.ParseExact(dateString, dateFormat, CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string Text { get; set; }
+    }
+}
