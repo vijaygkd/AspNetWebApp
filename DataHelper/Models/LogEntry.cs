e2e573cac448d9015d597acd244f52278ffe9223
Vijay Gaikwad <vig@microsoft.com>
2016-10-13 18:27:50 -0700

    move logEntry class to datahelper

---
+++ DataHelper/Models/LogEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileHelpers;
+
+namespace DataHelper
+{
+    [DelimitedRecord(","), IgnoreFirst(1)]
+    public class LogEntry
+    {
+        public int Day;
+
+        [FieldQuoted()]
+        public string Month;
+
+        public int Year;
+
+        //public DateTime Date
+        //{
+        //    get
+        //    {
+        //        var dateFormat = "d MMMM YYYY";
+        //        var dateString = $"{this.Day} {this.Month} {this.Year}";
+        //        return DateTime.ParseExact(dateString, dateFormat, CultureInfo.InvariantCulture);
+        //    }
+        //}
+
+        [FieldQuoted()]
+        public string Text;
+    }
+}
